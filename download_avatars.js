26bb7c649cdcade95c62ea083575425286a21c0a
ryan andrew sibat <ryanandrew.sibat@gmail.com>
2019-02-11 23:08:08 +0000

    updated download-avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,23 @@
 var request = require('request');
+var gitToken = require('./secrets.js');
 
 console.log('Welcome to the Github Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, callback) {
+  var option = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': gitToken.GITHUB_TOKEN
+    }
+  }
+
+  request(option, function(err, res, body) {
+    callback(err, body);
+  });
+};
+
+getRepoContributors('jquery', 'jquery', function(err, result){
+  console.log('Errors: ', err);
+  console.log('Results: ', result);
+});
